44cd31d511efc4ee6c0d2280877ea65313bf8003
Keenan Reed <reed.keenan91@gmail.com>
2020-02-17 21:28:07 -0500

    - Adding a new ShoppingCart Controller to handle Shopping methods. - Change the parameter for ShoppingCart from int to int? to allow null values - Added an if statement to see if the ID is null or not.

--- SnappyCart/Controllers/HomeController.cs
+++ SnappyCart/Controllers/HomeController.cs
@@ -3,15 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using SnappyCart.Models;
 
 namespace SnappyCart.Controllers
 {
     public class HomeController : Controller
     {
-        SnappyDBDataContext Dc = new SnappyDBDataContext();
-        userProduct Up = new userProduct();//create object Up that represents userProduct table in DB
-
         public ActionResult Index()
         {
             return View();
@@ -40,23 +36,6 @@ namespace SnappyCart.Controllers
         {
             ViewBag.Message = "Registration Page";
             return View();
-        }
-
-        [HttpGet]
-        /// get int ID 
-        /// look at db to see if user id exists, if exists grab all related data
-        /// store each record of data into the list
-        /// display list
-        
-        public ActionResult ShoppingCart(int ID)
-        {
-            List<userProduct> getUsersCart = new List<userProduct>();
-           var getResults = Dc.userProducts.Where(cart => cart.UserID == ID).ToList();
-            foreach(var item in getResults)
-            {
-                getUsersCart.Add(item);
-            }
-            return View(getUsersCart);
         }
     }
 }
---
+++ SnappyCart/Controllers/ShoppingController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SnappyCart.Models;
+
+namespace SnappyCart.Controllers
+{
+    public class ShoppingController : Controller
+    {
+
+        SnappyDBDataContext Dc = new SnappyDBDataContext();
+        userProduct Up = new userProduct();//create object Up that represents userProduct table in DB
+
+        // GET: Shopping
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ShoppingCart(int? ID) //Nullable Int Type 
+        {
+            List<userProduct> getUsersCart = new List<userProduct>();
+
+            /// get int ID 
+            /// look at db to see if user id exists, if exists grab all related data
+            /// store each record of data into the list
+            /// display list
+            if (ID == null)
+            {
+                return View();
+            }
+            else
+            {
+                var getResults = Dc.userProducts.Where(cart => cart.UserID == ID).ToList();
+                foreach (var item in getResults)
+                {
+                    getUsersCart.Add(item);
+                }
+                return View(getUsersCart);
+            }
+        }
+    }
+}
