2aed91c9b3492df414786ac4bb83064ced7fe97b
Mark Palmer <mark.palmer@itsalllive.com>
2020-02-23 14:43:00 -0500

    added getresults to usercontroler but its not working

--- SnappyCart/Controllers/UserController.cs
+++ SnappyCart/Controllers/UserController.cs
@@ -3,15 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SnappyCart.Models;
+
 
 namespace SnappyCart.Controllers
 {
     public class UserController : Controller
     {
+        SnappyDBDataContext Dc = new SnappyDBDataContext();
+        Users U = new Users();
+
         // GET: User
         public ActionResult UserProfile()
         {
             return View();
+        }
+
+        public ActionResult UserProfile(int? ID) //Nullable Int Type 
+        {
+            List<Users> getUsers = new List<Users>();
+
+            /// get int ID 
+            /// look at db to see if user id exists, if exists grab all related data
+            /// store each record of data into the list
+            /// display list
+            if (ID == null)
+            {
+                return View();
+            }
+            else
+            {
+                var getResults = U.Users.Where(Users => User.UserId == ID).ToList();
+                foreach (var item in getResults)
+                {
+                    getUsers.Add(item);
+                }
+                return View(getUsers);
+            }
         }
     }
 }
