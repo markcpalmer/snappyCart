37d75285a21c0460b1be4d4b6c89c3d178fe2522
Mark Palmer <mark.palmer@itsalllive.com>
2020-02-23 22:38:20 -0500

    changed name of the lambda

--- SnappyCart/Controllers/UserController.cs
+++ SnappyCart/Controllers/UserController.cs
@@ -33,7 +33,7 @@ namespace SnappyCart.Controllers
             }
             else
             {
-                var getResults = Dc.users.Where(Users => Users.UserID == ID).ToList();
+                var getResults = Dc.users.Where(cart => cart.UserID == ID).ToList();
                 foreach (var item in getResults)
                 {
                     getUsers.Add(item);
