9d4d35f48d124a68661fad3c50c1c94cc488ca0c
Mark Palmer <mark.palmer@itsalllive.com>
2020-03-01 23:40:30 -0500

    added variables to get and set in userProduct

--- SnappyCart/Models/UserProductModel.cs
+++ SnappyCart/Models/UserProductModel.cs
@@ -5,10 +5,19 @@ using System.Web;
 
 namespace SnappyCart.Models
 {
+
     public class UserProductModel
     {
+        //@*UserID,b.ProductID,OrderDate,ProductName, ProductDescription, ProductPrice*@
+
         public int userID { get; set; }
         public int productId { get; set; }
         public DateTime orderDate { get; set; }
+
+        public string productName { get; set; }
+        public string ProductDescription { get; set; }
+        public decimal ProductPrice { get; set; }
+
+
     }
 }
