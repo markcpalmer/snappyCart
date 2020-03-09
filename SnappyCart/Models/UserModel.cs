821e14121a7e439c22a0e89b5dc57b4a8870fd42
Mark Palmer <mark.palmer@itsalllive.com>
2020-03-08 23:11:36 -0400

    added require to input fields in register

--- SnappyCart/Models/UserModel.cs
+++ SnappyCart/Models/UserModel.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace SnappyCart.Models
 {
     public class UserModel
     {
+        [Required]
         public int UserId { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
         public string UserName { get; set; }
         public string LastName { get; set; }
         public string FirstName { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
